9ce42ef00803033f69fabebd53d838a197583745
Koray Birand <kbirand@mac.com>
2019-06-12 03:01:57 +0300

    Initial Commit

---
+++ enum/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  enum
+//
+//  Created by Koray Birand on 12.06.2019.
+//  Copyright © 2019 Koray Birand. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
